59118bdd42721153775e09e19bc4cd95b7ba0240
MateuszBar <matibaran3141@gmail.com>
2022-03-08 19:34:53 +0000

    Add to batch_consumer

--- API/batch_consumer.py
+++ API/batch_consumer.py
@@ -1,7 +1,54 @@
+from time import sleep
 from kafka import KafkaConsumer
+import threading
+from queue import Queue, Empty
 
-batch_consumer = KafkaConsumer('PinterestTopic',
-                bootstrap_servers=['localhost:9092'],
-                auto_offset_reset='earliest',
-                enable_auto_commit=True,
-                )
+message_queue = Queue()
+
+
+class Consumer(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        
+    def run(self):
+        consumer = KafkaConsumer(
+            "user_signups",
+            bootstrap_servers=["localhost:9092"],
+            auto_offset_reset="earliest",
+            enable_auto_commit=True,
+        )
+        
+        for message in consumer:
+            self.process_message(message.value)
+            
+        consumer.close()
+        
+    def process_message(self, message):
+        # Insert the message into buffer queue
+        message_queue.put(message)
+        
+        
+def process_messages():
+    print("processing message in queue buffer")
+    temp_messages = []
+    
+    try:
+        while True:
+            temp_messages.append(message_queue.get_nowait())
+
+    except Empty:
+        pass
+      
+    # Combine all messages into 1 call
+    print(temp_messages)
+    
+if __name__ == "__main__":
+    print("starting batch consumer worker")
+    
+    consumer = Consumer()
+    consumer.daemon = True
+    consumer.start()
+    
+    while True:
+        process_messages()
+        sleep(5)

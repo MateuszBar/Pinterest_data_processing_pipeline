b23fe405e4ebd0a196c39804aa82f1bfc4c830b9
MateuszBar <matibaran3141@gmail.com>
2022-06-17 14:53:42 +0100

    Add functions for data cleaning

--- API/spark_batch.py
+++ API/spark_batch.py
@@ -4,8 +4,23 @@ import operator
 import boto3 
 import json
 import configparser
+import findspark
 import os
+from pyspark.sql.functions import col,lit,split
 from pyspark import SparkContext
+from pyspark.sql.functions import UserDefinedFunction
+from pyspark.sql.types import StringType,FloatType,IntegerType,StructType,StructField,ArrayType,BooleanType
+
+#setting up s3 bucket
+s3 = boto3.resource('s3')
+# get a handle on the bucket that holds your file
+bucket = s3.Bucket('pinterestprojectpipeline')
+# get a handle on the object you want (i.e. your file)
+obj = bucket.Object(key='PinterestData1.json')
+# get the object
+response = obj.get()
+
+findspark.init()
 
 config = configparser.ConfigParser()
 config.read(os.path.expanduser("~/.aws/credentials"))
@@ -17,16 +32,49 @@ cfg = (
     pyspark.SparkConf()
     .setMaster(f"local[{multiprocessing.cpu_count()}]")
     .setAppName("pinterestpipeline_batchprocessing")
+    .set("spark.eventLog.enabled", False)
     .set("spark.executor.memory", "1g")
 )
 
+#creating session
 session = pyspark.sql.SparkSession.builder.config(conf=cfg).getOrCreate()
+sc = session.sparkContext
 
-hadoop_conf=session._jsc.hadoopConfiguration()
-hadoop_conf.set("fs.s3.impl", "org.apache.hadoop.fs.s3.S3FileSystem")
-hadoop_conf.set("fs.s3.awsAccessKeyId", access_id)
-hadoop_conf.set("fs.s3.awsSecretAccessKey", access_key)
-hadoop_conf.set("fs.s3.endpoint", "s3.amazonaws.com")
+df = session.createDataFrame(json.loads(response['Body'].read().decode('utf-8')))
 
-df=session.SparkContext.read.json("s3://pinterestprojectpipeline/PinterestData*.json")
-df.show()
+#function to change normalise is_image_or_video column
+def fix_is_image_or_video(x) -> StringType():
+    if x == 'multi-video(story page format)':
+        return 'video'
+    elif x == 'image':
+        return 'image'
+    else:
+        return x
+#create udf for is_image_or_video function
+udf_img_or_vid = UserDefinedFunction(lambda x:fix_is_image_or_video(x), StringType())
+
+#function change follower count to integer
+def fix_follower_count(x) -> float:
+    if type(x) == float or type(x) == int:
+        return x
+    if 'k' in x:
+        if len(x) > 1:
+            return float(x.replace('k', '')) * 1000
+        return 1000.0
+    if 'm' in x:
+        if len(x) > 1:
+            return float(x.replace('m', '')) * 1000000
+        return 1000000.0
+
+udf_fix_follower_count = UserDefinedFunction(lambda x:fix_follower_count(x), FloatType())
+
+udf_fix_tag_list = UserDefinedFunction(lambda x:x.split(','), ArrayType(StringType()))
+
+#apply functions to dataframe
+new_df = df.withColumn("is_image_or_video", udf_img_or_vid(col("is_image_or_video")))
+new_df = new_df.withColumn("follower_count", udf_fix_follower_count(col("follower_count")))
+new_df = new_df.withColumn("tag_list", udf_fix_tag_list(col("tag_list")))
+
+new_df.show()
+
+new_df.printSchema()

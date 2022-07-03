2ed3264ec49c48d5e2e3a3ccdbacbc662d30e769
MateuszBar <matibaran3141@gmail.com>
2022-07-03 21:04:42 +0100

    Integrate kafka spark streaming

--- API/streaming_consumer.py
+++ API/streaming_consumer.py
@@ -1,18 +1,51 @@
 from email.header import decode_header
 from kafka import KafkaConsumer
 from time import sleep
+import os
+from pyspark.sql import SparkSession
+import pyspark
+import multiprocessing
 
-def process_message(message):
-    print(message)    
-    
-if __name__ == "__main__":
-    print("starting streaming consumer app")
-    consumer = KafkaConsumer(
-        'PinterestTopic',
-        bootstrap_servers=["localhost:9092"],
-        group_id="group1"
-    )
-    
-    for message in consumer:
-        decoded_message = message.value.decode('utf-8')
-        process_message(decoded_message)
+# Download spark sql kakfa package from Maven repository and submit to PySpark at runtime. 
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.1 pyspark-shell'
+# specify the topic we want to stream data from.
+kafka_topic_name = "PinterestTopic"
+# Specify your Kafka server to read data from.
+kafka_bootstrap_servers = 'localhost:9092'
+
+cfg = (
+    pyspark.SparkConf()
+    .setMaster(f"local[{multiprocessing.cpu_count()}]")
+    .setAppName("pinterestpipeline_streaming")
+    .set("spark.eventLog.enabled", False)
+    .set("spark.executor.memory", "1g")
+)
+
+spark = SparkSession \
+        .builder \
+        .appName("KafkaStreaming")\
+        .config(conf=cfg)\
+        .getOrCreate()
+
+# Only display Error messages in the console.
+spark.sparkContext.setLogLevel("ERROR")
+
+# Construct a streaming DataFrame that reads from topic
+stream_df = spark \
+        .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
+        .option("subscribe", kafka_topic_name) \
+        .option("group_id","group1") \
+        .option("startingOffsets", "earliest") \
+        .load()
+
+# Select the value part of the kafka message and cast it to a string.
+stream_df = stream_df.selectExpr("CAST(value as STRING)")
+
+# outputting the messages to the console 
+stream_df.writeStream \
+    .format("console") \
+    .outputMode("append") \
+    .start()\
+    .awaitTermination()

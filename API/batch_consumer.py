44684b6e8783c40e2b8c29d29c37347fea55d659
MateuszBar <matibaran3141@gmail.com>
2022-07-04 20:47:21 +0100

    Fix comments

--- API/batch_consumer.py
+++ API/batch_consumer.py
@@ -3,9 +3,11 @@ from kafka import KafkaConsumer
 import boto3
 import json
 
+#initialise s3 client
 botoclient = boto3.client('s3')
 
 if __name__ == "__main__":
+    #initialise consumer
     print("starting batch consumer app")
     consumer = KafkaConsumer(
         'PinterestTopic',
@@ -13,11 +15,15 @@ if __name__ == "__main__":
         group_id="group1"
     )
     counter = 1
+    #create empty message queue
     message_queue = []
+    #for loop for each message in consumer
     for message in consumer:
+        #decode message and add to queue
         decoded_message = json.loads(message.value.decode('utf-8'))
         message_queue.append(decoded_message)
         print(f"{len(message_queue)} messages in queue")
+        #if queue is 300, send to s3 bucket
         if len(message_queue) > 299:
             print("sending messages to s3 bucket\n")
             botoclient.put_object(
@@ -25,6 +31,7 @@ if __name__ == "__main__":
                 Bucket='pinterestprojectpipeline',
                 Key=f'PinterestData{counter}.json'
             )
+            #reset queue and print counter
             message_queue.clear()
             print(f"counter = {counter}")
             counter += 1

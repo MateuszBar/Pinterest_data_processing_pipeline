2d6776bd3103cf6b145c3b333ff5a42da6f4bd5d
MateuszBar <matibaran3141@gmail.com>
2022-07-04 20:53:52 +0100

    Fix comments

--- API/spark_DAG.py
+++ API/spark_DAG.py
@@ -9,8 +9,10 @@ from cassandra.cluster import Cluster
 import json
 import os
 
+#initialise cassandra hook
 cassandra_hook = CassandraHook("cassandra_default")
 
+#define airflow schedule for this DAG
 default_args = {
     'owner': 'mateuszbar',
     'depends_on_past': False,
@@ -23,24 +25,34 @@ default_args = {
     'end_date': datetime(2024, 1, 1)
 }
 
+#function to get json data files from storage directory 
 def get_json_data(**context):
+    #define task instance
     task_instance = context["ti"]
+    #define storage directory
     path_to_json = '/home/mateusz/apache-cassandra-3.11.13/temp_pinterestdata/'
+    #find all files ending with .json
     json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
     file_path = path_to_json + json_files[0]
     json_data = []
+    #read json data from file and send to task instance
     for line in open(file_path, 'r'):
         json_data.append(json.loads(line))
     task_instance.xcom_push("json_data", json_data)
 
+#function that inserts data from context into cassandra table
 def insert_into_cassandra_db(**context):
+    #define task instance
     task_instance = context["ti"]
+    #pull data from task instance
     pinterest_data = task_instance.xcom_pull(task_ids='get_json_data', 
                                             key='json_data'
                                             )
+    #define cluster object and connect
     cluster = Cluster(['127.0.0.1'], port = 9042)
     session = cluster.connect('data')
 
+    #for loop to insert each record into cassandra table
     for i in range(0, len(pinterest_data)):
         try:
             session.execute(
@@ -57,6 +69,7 @@ def insert_into_cassandra_db(**context):
             continue
         print("Done Sending data")
 
+#define DAG with custom arguments
 with DAG(dag_id='daily_spark_job',
          default_args=default_args,
          schedule_interval='@hourly',
@@ -64,14 +77,19 @@ with DAG(dag_id='daily_spark_job',
          tags=['pinterest','pipeline', 'spark']
          ) as dag:
     
+    #run get_json_data task from cassandra storage directory
     run_spark_job = BashOperator(
         task_id='spark_job',
         bash_command='cd /home/mateusz/apache-cassandra-3.11.13 && python3 /home/mateusz/Coding/Coding/API/spark_batch.py')
+    #run get_json_data task to load data into instance
     run_get_json_data = PythonOperator(
         task_id='get_json_data',
         python_callable=get_json_data)
+    #run insert_into_cassandra_db task to insert instance data into cassandra table
     run_insert_into_cassandra_db = PythonOperator(
         task_id='insert_into_cassandra_db',
         python_callable=insert_into_cassandra_db)
-            
+    
+    #connect tasks
     run_spark_job >> run_get_json_data >> run_insert_into_cassandra_db
+    
